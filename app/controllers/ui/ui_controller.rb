0897055e7856e0c02de7722ebd02ec90f365cbf4
frank1234211 <frank1234211@gmail.com>
2015-12-05 16:35:53 +0800

    add line chart

--- app/controllers/ui/ui_controller.rb
+++ app/controllers/ui/ui_controller.rb
@@ -2,13 +2,15 @@ module UI
 module Show
   def self.registered(app)
     app_get_show = lambda do
-      @item = params[:item]
+    #  @item = params[:item]
+    @item="s"
       if @item
         logger.info 'redirected to get items'
         redirect "/show/#{@item}"
         return nil
       end
       logger.info 'load show'
+      logger.info @item
       slim :main_pane
     end
 
@@ -21,10 +23,16 @@ module Show
           session[:results] = nil
         else
           request_url = get_api_url("queenshop/#{@item}")
+          logger.info '+++++++++++'
+          logger.info request_url
           options =  { headers: { 'Content-Type' => 'application/json' } }
-          @results = HTTParty.get(request_url, options)
+      #    @results = HTTParty.get(request_url, options)
         end
+        logger.info "after HTTParty.get"
         @products = @results
+        @linechartdata={"100~200"=>4,"200~300"=>6,"300~400"=>8}
+        logger.info "after linechartdata"
+        logger.info @linechartdata
         slim :list_results
       rescue => e
         logger.info e
