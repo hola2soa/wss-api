41907d198a549d435778ae1cfe6bb360bc609768
frank1234211 <frank1234211@gmail.com>
2016-01-10 20:28:39 +0800

    add link in left pane and add link in item title

--- app/controllers/ui/ui_controller.rb
+++ app/controllers/ui/ui_controller.rb
@@ -26,57 +26,82 @@ module Show
 
     app_get_show_item = lambda do
       begin
-        @item = params[:item]
+        @item = params[:item] # @item is keyword or category
         logger.info @item
+
+        options =  { headers: { 'Content-Type' => 'application/json' }}
+
         if session[:results]
           @results = session[:results]
           session[:results] = nil
-        else
-          options =  { headers: { 'Content-Type' => 'application/json' } }
+        elsif @item == 'pants' #category
+          request_url = get_api_url("?store=queenshop&category=pants")
+          @results1 = HTTParty.get(request_url, options)
+        elsif @item == 'popular' #category
+          request_url = get_api_url("?store=queenshop&category=popular")
+          @results1 = HTTParty.get(request_url, options)
+          request_url = get_api_url("?store=stylemooncat&category=popular")
+          @results2 = HTTParty.get(request_url, options)
+        elsif @item == 'accessories' #category
+          request_url = get_api_url("?store=queenshop&category=accessories")
+          @results1 = HTTParty.get(request_url, options)
+          request_url = get_api_url("?store=stylemooncat&category=accessories")
+          @results2 = HTTParty.get(request_url, options)
+        elsif @item == 'tops' #category
+          request_url = get_api_url("?store=queenshop&category=tops")
+          @results1 = HTTParty.get(request_url, options)
+          request_url = get_api_url("?store=stylemooncat&category=tops")
+          @results2 = HTTParty.get(request_url, options)
+        elsif @item == 'latest' #category
+          request_url = get_api_url("?store=queenshop&category=latest")
+          @results1 = HTTParty.get(request_url, options)
+          request_url = get_api_url("?store=stylemooncat&category=latest")
+          @results2 = HTTParty.get(request_url, options)
+        else #keyword
           request_url = get_api_url("?store=queenshop&keyword=#{URI.escape(@item)}")
           @results1 = HTTParty.get(request_url, options)
           request_url = get_api_url("?store=joyceshop&keyword=#{URI.escape(@item)}")
-          logger.info request_url
           @results2 = HTTParty.get(request_url, options)
           request_url = get_api_url("?store=stylemooncat&keyword=#{URI.escape(@item)}")
-          logger.info request_url
           @results3 = HTTParty.get(request_url, options)
-          logger.info request_url
         end
 
         @products = []
         #combine results
-        @results1.each { |result| @products.push(result)}
-        @results2.each { |result| @products.push(result)}
-        @results3.each { |result| @products.push(result)}
+        @results1.each { |result| @products.push(result)} if @results1!=nil
+        @results2.each { |result| @products.push(result)} if @results2!=nil
+        @results3.each { |result| @products.push(result)} if @results3!=nil
 
         #intialize counter
-        @count_0_300 =0
-        @count_300_600 =0
-        @count_600_900 =0
-        @count_900_1200 =0
-        @count_1200_up =0
+        @count_0_200 =0
+        @count_200_400 =0
+        @count_400_600 =0
+        @count_600_800 =0
+        @count_800_1000 =0
+        @count_1000_up =0
 
         #count number of items each range of price
         @products.each do |product|
           if product["price"].to_i >0
-            if product["price"].to_i <300
-              @count_0_300 = @count_0_300 + 1
+            if product["price"].to_i <200
+              @count_0_200 += 1
+            elsif product["price"].to_i <400
+              @count_200_400 += 1
             elsif product["price"].to_i <600
-              @count_300_600 = @count_300_600 + 1
-            elsif product["price"].to_i <900
-              @count_600_900 = @count_600_900 + 1
-            elsif product["price"].to_i <1200
-              @count_900_1200 = @count_900_1200 + 1
+              @count_400_600 += 1
+            elsif product["price"].to_i <800
+              @count_600_800 += 1
+            elsif product["price"].to_i <1000
+              @count_800_1000 += 1
             else
-              @count_1200_up = @count_1200_up + 1
+              @count_1000_up += 1
             end
           end
         #  logger.info "#{@count_0_300}  #{@count_300_600}  #{@count_600_900}  #{@count_900_1200}  #{@count_1200_up}"
         end
         # " "=>0 is used to adjust appearance
-        @columnchartdata={"0~300"=>@count_0_300,"300~600"=>@count_300_600,"600~900"=>@count_600_900,
-          "900~1200"=>@count_900_1200,"1200 up"=>@count_1200_up," "=>0}
+        @columnchartdata={"0~200"=>@count_0_200,"200~400"=>@count_200_400,"400~600"=>@count_400_600,
+          "600~800"=>@count_600_800,"800~1000"=>@count_800_1000,"1000 up"=>@count_1000_up," "=>0}
         slim :list_results
       rescue => e
         logger.info e
